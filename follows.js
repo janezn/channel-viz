f9f95edc4b29ba0be979144cf2823b5beb7aaa61
janezn <narobejanez@gmail.com>
2013-12-27 21:44:40 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,11 +12,11 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+	var defaultKey		= 'SKBZW79Hys2LVoFDiFB9Owv0mY9VELhG41ptoVHlRa2xqP2A', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [13696819], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'JN HomeBrew System', // Replaces Xively logo in the header
+		dataDuration	= '3600', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
